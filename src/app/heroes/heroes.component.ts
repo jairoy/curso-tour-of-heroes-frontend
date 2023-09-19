fff183cdea56cfa20c7a1066983611701080bc04
jairoy <jairo.yenecura@gmail.com>
2023-09-19 10:10:04 -0300

    aula 9 - Inclusao ESlint e aula 10 - lista de Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { Hero } from './model.hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Woverine'
   };
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 
 }

9249824eecfe5e2747391f68d8239bf640e28602
jairoy <jairo.yenecura@gmail.com>
2023-09-21 13:39:03 -0300

    aula 13 - Implementacao Hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './model.hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +8,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
-  heroes = HEROES;
-
+export class HeroesComponent implements OnInit{
+  heroes : Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() : void{
+    this.getHeroes();
+
+  }
+
+  getHeroes() : void{
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;

9b3571bf4a5166c5d366d0c0596bbe4206463f21
jairoy <jairo.yenecura@gmail.com>
2023-09-29 08:28:10 -0300

    aula 19 - Navegacao do detalhe do hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './model.hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,25 +8,16 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit{
-  heroes : Hero[] = [];
-  selectedHero?: Hero;
+  heroes : Hero[] = []; 
 
-  constructor(private heroService: HeroService, 
-              private messageService: MessageService) {}
+  constructor(private heroService: HeroService){}
 
   ngOnInit() : void{
     this.getHeroes();
-
   }
 
   getHeroes() : void{
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero Id=${hero.id}`);
-    
   }
 
 }
